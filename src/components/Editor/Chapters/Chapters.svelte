50ac3b3c1d514ec9a0c766b5aab955b0ba6e3d7d
Steven Bell <thebells1111@gmail.com>
2021-10-03 17:11:38 -0500

    making chapters more functional

--- src/components/Editor/Chapters/Chapters.svelte
+++ src/components/Editor/Chapters/Chapters.svelte
@@ -1,148 +1,167 @@
 <script>
-  import { editingEpisode, rssData, player } from "../../../../stores/editor";
+	import convertTime from '$lib/functions/convertTime';
+	import { browser } from '$app/env';
 
-  let blankChapters = {
-    "@type": "application/json",
-    "@_url": "",
-  };
+	import { player } from '$/stores';
 
-  let chapters;
+	import { editingEpisode, rssData } from '$/editor';
+	import ChapterImage from './ChapterImage.svelte';
 
-  // $: console.log($editingEpisode?.['podcast:chapters']);
-  // $: console.log(chaptersArray);
+	let blankChapters = {
+		'@type': 'application/json',
+		'@_url': ''
+	};
 
-  $: if ($editingEpisode) {
-    chapters = $editingEpisode["podcast:chapters"]
-      ? $editingEpisode?.["podcast:chapters"]
-      : blankChapters;
-  }
-  let editingChapters = [];
+	let chapters;
 
-  function loadChapters() {
-    if (chapters["@_url"]) {
-      fetch("/api/httpsproxy.js?url=" + encodeURIComponent(chapters["@_url"]))
-        .then((x) => {
-          return x.json();
-        })
-        .then((y) => {
-          editingChapters = y.chapters
-            .concat(editingChapters)
-            .sort(function (a, b) {
-              return a.startTime - b.startTime;
-            });
+	// $: console.log($editingEpisode?.['podcast:chapters']);
+	// $: console.log(chaptersArray);
 
-          console.log(editingChapters);
-        });
-    } else {
-      editingChapters = editingChapters;
-    }
-  }
+	$: if ($editingEpisode) {
+		chapters = $editingEpisode['podcast:chapters']
+			? $editingEpisode?.['podcast:chapters']
+			: blankChapters;
+	}
+	let editingChapters = [];
 
-  function markChapter() {
-    editingChapters.push({
-      img: "",
-      startTime: $player.currentTime(),
-      title: "New Chapter",
-      url: "",
-    });
-    editingChapters = editingChapters.sort(function (a, b) {
-      return a.startTime - b.startTime;
-    });
-  }
+	function getUniqueListBy(arr, key) {
+		return [...new Map(arr.map((item) => [item[key], item])).values()];
+	}
 
-  function convertTime(time) {
-    try {
-      let formatTime = new Date(time * 1000).toISOString().substr(11, 8);
-      if (formatTime.charAt(0) === "0") {
-        return formatTime.substring(1);
-      }
+	function loadChapters() {
+		if (chapters['@_url']) {
+			fetch(`/api/httpsproxy?url=` + encodeURIComponent(chapters['@_url']))
+				.then((x) => {
+					return x.json();
+				})
+				.then((y) => {
+					editingChapters = getUniqueListBy(y.chapters.concat(editingChapters), 'startTime').sort(
+						function (a, b) {
+							return a.startTime - b.startTime;
+						}
+					);
 
-      return formatTime;
-    } catch {
-      (err) => console.log(err);
-    }
-  }
+					console.log(editingChapters);
+				});
+		} else {
+			editingChapters = editingChapters;
+		}
+	}
+
+	function markChapter() {
+		editingChapters = editingChapters = getUniqueListBy(
+			editingChapters.concat({
+				title: 'New Chapter',
+				url: '',
+				img: '',
+				startTime: $player.currentTime()
+			}),
+			'startTime'
+		).sort(function (a, b) {
+			return a.startTime - b.startTime;
+		});
+		console.log(editingChapters);
+	}
+	$: if (browser && chapters) {
+		loadChapters();
+	}
 </script>
 
 {#if $rssData}
-  <div>
-    Chapters Link:
-    <label>
-      <h4>URL:</h4>
-      <input type="text" bind:value={chapters["@_url"]} />
-    </label>
-  </div>
-  <button on:click={loadChapters}>Load Chapters</button>
-  <button on:click={markChapter}>Mark New Chapter</button>
+	<header>
+		<div>
+			<h4>Chapters File URL:</h4>
+			<button on:click={loadChapters}> Load Chapters </button>
+		</div>
+
+		<input type="text" bind:value={chapters['@_url']} />
+	</header>
+
+	<button on:click={markChapter}>Mark New Chapter</button>
 {/if}
 
 <ul>
-  {#each editingChapters as chapter, i}
-    <li>
-      <p>{i + 1}.</p>
-      <img src={chapter.img} />
-      <div class="middle">
-        {chapter.title}
-        <!-- {chapter.url} -->
-      </div>
-      <div class="time">{convertTime(chapter.startTime)}</div>
-    </li>
-  {/each}
+	{#each editingChapters as chapter, i}
+		<li>
+			<p>{i + 1}.</p>
+			<ChapterImage {chapter} />
+			<div class="middle">
+				<label>Title: <input bind:value={chapter.title} /></label>
+				<label>URL: <input bind:value={chapter.url} /></label>
+
+				<!-- {chapter.url} -->
+			</div>
+			<div class="time">{convertTime(chapter.startTime)}</div>
+		</li>
+	{/each}
 </ul>
 
 <style>
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 8px;
-  }
-  h5 {
-    margin: 0;
-    padding: 0;
-  }
-  label {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-  }
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+		margin-bottom: 8px;
+	}
 
-  input {
-    flex-grow: 1;
-    padding: 8px;
-  }
-  ul {
-    width: calc(100% - 16px);
-  }
+	header > div {
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+		align-items: flex-end;
+	}
+	h4 {
+		margin: 0;
+		padding: 0;
+	}
 
-  li {
-    width: 100%;
-    margin: 0 8px 8px 8px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-  }
+	input {
+		width: 100%;
+		padding: 8px;
+	}
+	ul {
+		width: calc(100% - 16px);
+	}
 
-  li > p {
-    padding: 0 8px 0 0;
-    align-self: flex-start;
-  }
+	li {
+		width: 100%;
+		margin: 0 8px 8px 8px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		position: relative;
+	}
 
-  .middle {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    padding: 0 8px;
-  }
+	li > p {
+		padding: 0 8px 0 0;
+		align-self: flex-start;
+	}
 
-  .time {
-  }
+	.middle {
+		flex-grow: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		padding: 0 8px;
+	}
 
-  li > img {
-    width: 100px;
-  }
+	.middle > label {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		position: relative;
+	}
+	.middle > label > input {
+		position: relative;
+		top: 4px;
+		padding: 4px;
+		margin-left: 8px;
+	}
+	.time {
+	}
 </style>
